095b007d0d99a8be08203e504e5849be65974b0c
HalilHamarat <hamarathalil5261@icloud.com>
2022-10-08 11:57:40 +0300

    Create matematiksel_operatorler.py

---
+++ matematiksel_operatorler.py
@@ -0,0 +1,34 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+
+#Toplama
+print(5487+9241)
+
+#Çıkartma
+print(564-8564)
+
+#Çarpma
+print(5*54)
+
+#Bölme
+print(54/685)
+
+#Üs Alma
+print(2**63)
+print(200**100*500)
+
+#Mod Alma
+print(40%3)
+
+#Tam Bölme
+print(40//3)  #13
+print(-40//3)  #-14
+
+
