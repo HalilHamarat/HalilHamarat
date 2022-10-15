a8747c5ef350e8992820e06133ded493a81cb49f
HalilHamarat <hamarathalil5261@icloud.com>
2022-10-15 11:30:04 +0300

    Create karakter_dizileri.py

---
+++ karakter_dizileri.py
@@ -0,0 +1,32 @@
+#KULLANICI YAŞ VE İSİM DEĞİŞKENLERİNİ OLUŞTURALIM
+yas = "14"
+isim = "Halil İbrahim Hamarat"
+
+print("Adınız: "+ isim + " ," + " Yaşınız: " + yas)
+
+#KARAKTER VERİSİNİN UZUNLUĞUNA BAKALIM
+print(len(isim))
+
+#str veri tipindeki değerin çeşitli elemanlarını yazdıralım
+#elemanlar 0'dan başlar
+print(isim[0])
+print(isim[1])
+print(isim[2])
+print(isim[3])
+print(isim[4])
+print(isim[5])
+print(isim[6])
+print(isim[7])
+print(isim[8])
+print(isim[9])
+print(isim[10])
+print(isim[11])
+print(isim[12])
+print(isim[13])
+print(isim[14])
+print(isim[15])
+print(isim[16])
+print(isim[17])
+print(isim[18])
+print(isim[19])
+print(isim[20])
