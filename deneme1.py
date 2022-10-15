37179ef6beefb4ad390b19d4f95c972d07358b3e
HalilHamarat <hamarathalil5261@icloud.com>
2022-10-15 11:33:16 +0300

    Create deneme1.py

---
+++ deneme1.py
@@ -0,0 +1,9 @@
+print (" ( 1 + 2) * 3 ) * 4")
+x = int( input( "Birinci sayıyı giriniz:") )
+y = int( input( "İkinci sayıyı giriniz:") )
+z = int( input( "Üçüncü sayıyı giriniz:") )
+g = int( input( "Dördüncü sayıyı giriniz:") )
+
+print( ( (x + y ) * z) * g)
+
+exit("Bitti")
