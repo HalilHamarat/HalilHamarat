341ae9542900af5a70620fc89f4861904832a17c
HalilHamarat <hamarathalil5261@icloud.com>
2022-10-22 11:47:39 +0300

    Create string_kacis_dizileri.py

---
+++ string_kacis_dizileri.py
@@ -0,0 +1,18 @@
+"""
+Python’ın gözünde bir başlangıç tırnağı ile bitiş tırnağı arasında yer alan
+her şey bir karakter dizisidir. Ancak bizler metin içerisinde bu tırnakları
+özel bir amaçla kullanmak isteyebiliriz. Bu tür durumlarda kaçış karakteri
+olan ters bölü işaretini (\) kullanarak hatadan kaçarız.
+"""
+
+print("Ahmet, \"Bugün sinemaya gidiyorum .\"dedi.")
+
+print('Yarın Adana\'ya gidiyorum.')
+
+print("C:\\Users\\Tasarım\\Documents\\GitHub\\HalilHamarat")
+
+#özel karakterler
+# \n = alt satır
+print("Halil \n İbrahim \nHamarat") #alt satır
+print("Halil \t İbrahim \t Hamarat")  #tab
+print("Halil \t İbrahim \t Hamarat") # tab
