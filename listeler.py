9f5c7271b584f145501753d3a89839661a4db02e
HalilHamarat <hamarathalil5261@icloud.com>
2022-10-22 11:47:24 +0300

    Update listeler.py

--- listeler.py
+++ listeler.py
@@ -1,17 +1,25 @@
 #boş bir liste tanımlayalım
-liste[]
+liste=[1,2,3,4,5,6,7,8,9]
 print(liste)
 print(type(liste))
 
 okul = "Sancaktepe Teknoloji Anadolu İmam Hatip Lisesi"
 
 kelimeler = okul.split()
-print(kelimeler[0]) #ilk kelime
-print(kelimeler[1])
-print(kelimeler[2])
-print(kelimeler[3])
-print(kelimeler[4])
-print(kelimeler[5])
+# print(kelimeler[0]) #ilk kelime
+# print(kelimeler[1])
+# print(kelimeler[2])
+# print(kelimeler[3])
+# print(kelimeler[4])
+# print(kelimeler[5])
 #print(kelimeler[6]) #hata verdi
 
+ad= "Halil İbrahim Hamarat"
+# print(ad[0]) #H
+# print(ad[0:]) #Halil İbrahim Hamarat
+# print(ad[6:])
+# print(ad[0:13])
+# print(ad[14:])
 
+print(ad[-7:-14:-1])
+print(ad[::-1]) 
