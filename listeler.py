4fe18eeaca2958e3a458cc668ce8910dd42a9340
HalilHamarat <hamarathalil5261@icloud.com>
2022-10-22 11:36:00 +0300

    Create listeler.py

---
+++ listeler.py
@@ -0,0 +1,17 @@
+#boş bir liste tanımlayalım
+liste[]
+print(liste)
+print(type(liste))
+
+okul = "Sancaktepe Teknoloji Anadolu İmam Hatip Lisesi"
+
+kelimeler = okul.split()
+print(kelimeler[0]) #ilk kelime
+print(kelimeler[1])
+print(kelimeler[2])
+print(kelimeler[3])
+print(kelimeler[4])
+print(kelimeler[5])
+#print(kelimeler[6]) #hata verdi
+
+
