83e5a446175e73aef36055d3146779eb56ba9b70
HalilHamarat <hamarathalil5261@icloud.com>
2022-12-24 11:05:54 +0300

    a

--- fonksiyon_parametreleri.py
+++ fonksiyon_parametreleri.py
@@ -1,25 +1,44 @@
-ad = input("adınız: ")
-soy = input("soyadınız: ")
-notlar = input("sınav notları (virgülle ayrılmış)")
+# Öğrencinin adını, soyadını ve notlarını alıp ortalamasını
+# dosyaya yazdıran bir program yazalım
 
-
-def ogrenci_kaydet(ad,soyad,notlar):
+# Öğrenci kaydetme fonksiyonu
+def ogrenci_kaydet(ad, soyad, notlar):
+    # notları integer yap ve ortalamayı hesapla
     notlar_list = [int(x) for x in notlar.split(",")]
     ortalama = sum(notlar_list) / len(notlar_list)
 
-    dosya = open("ogrenciler.txt", mode="a")
-    dosya.write(f"ad:{ad} , soyad:{soy} , notlar:{notlar} , ortalama:{notlar_list}")
+    # dosyaya yaz
+    dosya = open("öğrenciler.txt", mode="a", encoding="utf-8")
+    dosya.write(f"Ad: {ad}, Soyad: {soyad}, Notlar: {notlar}, Ortalama: {ortalama}\n")
     dosya.close()
 
+
+def ogrenci_kaydet2(ad, soyad, notlar=None):
+    if notlar:  # if isinstance(notlar, str):
+        # notları integer yap ve ortalamayı hesapla
+        notlar_list = [int(x) for x in notlar.split(",")]
+        ortalama = sum(notlar_list) / len(notlar_list)
+    else:
+        notlar = "Not bilgisi yok"
+        ortalama = "Ortalama hesaplanmadı"
+
+    # dosyaya yaz
+    dosya = open("öğrenciler.txt", mode="a", encoding="utf-8")
+    dosya.write(f"Ad: {ad}, Soyad: {soyad}, Notlar: {notlar}, Ortalama: {ortalama}\n")
+    dosya.close()
+
+
 while True:
-    ne_yapilacak = input("Ne yapacaksınız (1 kaydet) (0 çık)")
-
-
+    ne_yapilacak = input("Ne yapacaksınız? (1: Kaydet, 0: Çık): ")
 
     if ne_yapilacak == "0":
         break
 
-    ad = input("ad")
-    soyad = input("soyad")
-    notlar = input("not")
-    ogrenci_kaydet(ad, soyad, notlar)
+    ad = input("Öğrenci Adı: ")
+    soyad = input("Öğrenci Soyadı: ")
+    notlar = input("Sınav notları (virgülle ayrılmış): ")
+    if notlar != "":
+        ogrenci_kaydet2(ad, soyad, notlar)
+    else:
+        ogrenci_kaydet2(ad, soyad)
+
